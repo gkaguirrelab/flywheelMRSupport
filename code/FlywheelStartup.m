d35b40b87adfd531b15586f66614442b98f1d249
gkaguirre <gkaguirre@me.com>
2017-11-03 14:43:34 -0400

    First commit
    Signed-off-by: gkaguirre <gkaguirre@me.com>

---
+++ code/FlywheelStartup.m
@@ -0,0 +1,7 @@
+addpath('/Users/aguirre/Documents/MATLAB/toolboxes/JSONio');
+addpath('/Users/aguirre/Documents/MATLAB/src/flywheel.io/sdk/bridge/dist/matlab-binary');
+
+
+
+fw = Flywheel('yourAccessCodeHere');
+
