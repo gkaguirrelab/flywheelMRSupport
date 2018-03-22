1d494c0f4b4d6d10f26259e0c062e3f590ee8d77
David Brainard <brainard@psych.upenn.edu>
2018-03-22 14:07:26 -0400

    Add ‘nodownload’ key

--- code/getAnalysisFromFlywheel.m
+++ code/getAnalysisFromFlywheel.m
@@ -5,8 +5,12 @@ function [fwInfo] = getAnalysisFromFlywheel(theProject,analysisLabel,dataDownloa
 %  [analysis_id] = GetAnalysisFromFlywheel(theProject,analysisLabel,analysisScratchDir)
 %
 % Description:
-%	Use api to download data from flywheel from flywheel based an a given
-%   project name and analysis label.
+%	Use api to download data from flywheel based an a given
+%   project name and analysis label.  Returns a structure (fwInfo)
+%   that provides various useful information about the analysis.
+%
+%   If you just want the structure but don't want to download, set the
+%   value for the 'nodownload' key to true.
 %
 % Inputs:
 %  theProject             - Define the project. This is the name of the
@@ -20,22 +24,26 @@ function [fwInfo] = getAnalysisFromFlywheel(theProject,analysisLabel,dataDownloa
 %                           Analysis |" string on the analysis tab.
 %  analysisScratchDir     - This is a string specifying the directory where
 %                           this routine should put its files.
-% Optional key/value pairs:
-%  'verbose'              - Logical flag, default false
-%  'searchDir'            - Specify another location for the recursive file 
-%                           search to take place for skip download check: 
-%                           default is the dataDownloadDir location. 
-% Outputs:
-%   analysis_id           - The unique analysis id set by flywheel
-%   session_id            - The unique session id set by flywheel
 %
+% Outputs:
+%   fwInfo                - Structure with various fields providing
+%                           information about the analysis. 
+%
+% Optional key/value pairs:
+%  'verbose'              - Logical, default false
+%  'searchDir'            - Specify another location for the recursive file
+%                           search to take place for skip download check:
+%                           default is the dataDownloadDir location.
+%  'nodownload'           - Logical, default false. Set this to true if you
+%                           just want the fwInfo struct but do not want to
+%                           download the data.
 %
 % Examples are provided in the source code.
 %
 
 % History
 %  1/31/18  mab  Created from previous example script.
-%
+
 % Examples:
 %{
     % Downloads an analysis into tempdir
@@ -43,25 +51,26 @@ function [fwInfo] = getAnalysisFromFlywheel(theProject,analysisLabel,dataDownloa
     analysisLabel = 'fmriprep 02/09/2018 11:40:55';
     getAnalysisFromFlywheel(theProject,analysisLabel,tempdir);
 %}
-
+%{
+    % Just get analysis information
+    theProject = 'LFContrast';
+    analysisLabel = 'fmriprep 02/09/2018 11:40:55';
+    fwInfo = getAnalysisFromFlywheel(theProject,analysisLabel,tempdir,'nodownload',true)
+%}
 
 %% Parse vargin for options passed here
 p = inputParser; p.KeepUnmatched = false;
-
-% Required
 p.addRequired('theProject', @ischar);
 p.addRequired('analysisLabel', @ischar);
 p.addRequired('analysisScratchDir', @ischar);
-% Optional params
 p.addParameter('verbose',false, @islogical);
+p.addParameter('nodownload',false, @islogical);
 p.addParameter('searchDir', dataDownloadDir, @ischar);
-% parse
 p.parse(theProject, analysisLabel, dataDownloadDir, varargin{:})
 
 
 %% Open flywheel object
 fw = Flywheel(getpref('flywheelMRSupport','flywheelAPIKey'));
-
 
 %% Find out who we are
 me = fw.getCurrentUser();
@@ -125,67 +134,83 @@ end
 %   fw.downloadFileFromAnalysis(session_id, analysis_id, file_name, output_name)
 
 % Where do we want the files stored?
-if (~exist(dataDownloadDir,'dir'))
-    mkdir(dataDownloadDir);
+if (~p.Results.nodownload)
+    if (~exist(dataDownloadDir,'dir'))
+        mkdir(dataDownloadDir);
+    end
 end
-
 
 %% Set-up search structure and search
 searchStruct = struct('return_type', 'file', ...
     'filters', {{struct('term', ...
     struct('analysis0x2elabel', analysisLabel))}});
 results = fw.search(searchStruct);
+
+% Grab ids from first result. 
 analysis_id = results(1).analysis.x_id;
-[~,cmdout] = unixFind(analysis_id, p.Results.searchDir, 'searchCase', 'wildcard');
-if ~isempty(cmdout)
-    warning('flywheelMRSupport:analysisAlreadyPresent','WARNING: File found in search containing the analysis id: %s \n',results(1).analysis.x_id);
-else
-    % Iterate over results and download the files
-    for ii = 1:numel(results)
-        file_name = results(ii).file.name;
-        output_name = fullfile(dataDownloadDir, file_name);
-        
-        session_id = results(ii).session.x_id;
-        analysis_id = results(ii).analysis.x_id;
-        subject = results(ii).subject.code;
-        
-        if p.Results.verbose
-            fprintf('Downloading %dMB file: %s ... \n', round(results(ii).file.size / 1000000), file_name);
-            tic
-        end
-        
-        fw.downloadFileFromAnalysis(session_id, analysis_id, file_name, output_name);
-        
-        if p.Results.verbose
-            toc
-        end
-        
-        %
-        % We don't know quite what happens if we unzip more than one file, but
-        % sooner or later we will find out.
-        [~,~,ext] = fileparts(file_name);
-        unzipDir = fullfile(dataDownloadDir,[subject '_' analysis_id]);
-        switch (ext)
-            case '.zip'
-                if p.Results.verbose
-                    fprintf('Unzipping %s\n',output_name);
-                end
-                if (~exist(unzipDir,'dir'))
-                    mkdir(unzipDir);
-                end
-                system(['unzip -o ' output_name ' -d ' unzipDir]);
-                delete(output_name);
+session_id = results(1).session.x_id;
+subject = results(1).subject.code;
+
+% This next
+if (~p.Results.nodownload)
+    [~,cmdout] = unixFind(analysis_id, p.Results.searchDir, 'searchCase', 'wildcard');
+    if ~isempty(cmdout)
+        warning('flywheelMRSupport:analysisAlreadyPresent','WARNING: File found in search containing the analysis id: %s \n',results(1).analysis.x_id);
+    else
+        % Iterate over results and download the files
+        for ii = 1:numel(results)
+            file_name = results(ii).file.name;
+            output_name = fullfile(dataDownloadDir, file_name);
+            
+            % Check that our assumptions about what can change with
+            % iteration are met.
+            if (~strcmp(session_id,results(ii).session.x_id))
+                error('Session number changed with iteration');
+            end
+            if (~strcmp(analysis_id,results(ii).analysis.x_id))
+                error('Analysis id changed with iteration');
+            end
+            if (~strcnp(subject,results(ii).subject.code))
+            	error('Subject code changed with iteration');
+            end
+            
+            % Do the download
+            if p.Results.verbose
+                fprintf('Downloading %dMB file: %s ... \n', round(results(ii).file.size / 1000000), file_name);
+                tic
+            end
+            fw.downloadFileFromAnalysis(session_id, analysis_id, file_name, output_name);
+            if p.Results.verbose
+                toc
+            end
+            
+            % We don't know quite what happens if we unzip more than one file, but
+            % sooner or later we will find out.
+            [~,~,ext] = fileparts(file_name);
+            unzipDir = fullfile(dataDownloadDir,[subject '_' analysis_id]);
+            switch (ext)
+                case '.zip'
+                    if p.Results.verbose
+                        fprintf('Unzipping %s\n',output_name);
+                    end
+                    if (~exist(unzipDir,'dir'))
+                        mkdir(unzipDir);
+                    end
+                    system(['unzip -o ' output_name ' -d ' unzipDir]);
+                    delete(output_name);
+            end
         end
     end
 end
 
 %% Create flywheel info struct
-fwInfo.session_id    = results(1).session.x_id;
+fwInfo.session_id    = session_id;
 fwInfo.analysis_id   = analysis_id;
+fwInfo.subject       = subject;
 fwInfo.label         = results(1).session.label;
 fwInfo.timestamp     = results(1).session.timestamp;
-fwInfo.subject       = results(1).subject.code;
 fwInfo.project_id    = project_id;
 fwInfo.theProject    = theProject;
 fwInfo.analysisLabel = analysisLabel;
+
 end
