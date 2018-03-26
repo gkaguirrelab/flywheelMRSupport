e76c85c0b4a0e23e39b0663d44cd1d2576a94124
gkaguirre <gkaguirre@me.com>
2018-03-26 11:30:43 -0400

    Local hook paths now unix compliant
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- configuration/flywheelMRSupportLocalHookTemplate.m
+++ configuration/flywheelMRSupportLocalHookTemplate.m
@@ -30,21 +30,22 @@ setpref(toolboxName,'flywheelAPIKey',flywheelAPIKey);
 userID = strtrim(userID);
 switch userID
     case 'dhb'
-        baseDir = fullfile(filesep,'Volumes','Users1','Dropbox (Aguirre-Brainard Lab)');
+        baseDir = fullfile(filesep,'Volumes','Users1','Dropbox\ \(Aguirre-Brainard\ Lab\)');
         
         % Could put user specific things in, but at the moment generic
         % is good enough.
     otherwise
-        baseDir = fullfile('/Users',userID,'Dropbox (Aguirre-Brainard Lab)');
+        baseDir = fullfile('/Users',userID,'Dropbox\ \(Aguirre-Brainard\ Lab\)');
 end
+
 
 %% Paths to local applications needed to run this file
 if ismac
     % Code to run on Mac plaform
-    setpref(toolboxName,'binANTS',fullfile(baseDir,'FLYW_admin/MRI processing apps/ANTs/OSX_10.13.2/ANTs/bin/bin'));
+    setpref(toolboxName,'binANTS',fullfile(baseDir,'FLYW_admin/MRI\ processing\ apps/ANTs/OSX_10.13.2/ANTs/bin/bin'));
 elseif isunix
     % Code to run on Linux plaform
-    setpref(toolboxName,'binANTS',fullfile(baseDir,'FLYW_admin/MRI processing apps/ANTs/Ubuntu_16.04/ANTs/bin/bin'));
+    setpref(toolboxName,'binANTS',fullfile(baseDir,'FLYW_admin/MRI\ processing\ apps/ANTs/Ubuntu_16.04/ANTs/bin/bin'));
 elseif ispc
     % Code to run on Windows platform
     warning('No binaries for ANTs on PC')
