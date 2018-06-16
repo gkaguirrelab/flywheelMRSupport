32098c8fb98cbb0632dfe16726b0db7ce046e1cb
Nikhil Boddu <nikhilvamsi31@gmail.com>
2018-06-15 20:37:35 -0400

    Rename and convert to function
    Signed-off-by: Nikhil Boddu <nikhilvamsi31@gmail.com>

--- code/analyzeQCmetrics.m
+++ code/analyzeQCmetrics.m
@@ -1,8 +1,8 @@
-% getQCMetrics
-% Script to create plots of quality control metrics for a flywheel project
+function [] = analyzeQCmetrics(theProject,targetProjectField,varargin)
+% Script to create plots of quality control metrics and identify outlier data for a flywheel project
 %
 % Syntax:
-%  getQCMetrics
+%  analyzeQCMetrics(theProject,targetProjectField)
 %
 % Description:
 %   This routine connects to a Flywheel server and pulls quality assessment
@@ -12,10 +12,24 @@
 %   flywheelMRSupport toolbox.
 %
 % Inputs:
-%   none
-%
+%   theProject              - Define the project. This is the project you
+%                             are interested in performing a QC metric
+%                             analysis on. This must be a String value.
+%   targetProjectField      - Define the target location of the output
+%                             files for this analysis. You may specify
+%                             either "analyses" or "files".
+%                            
 % Outputs:
 %   none
+%
+% Optional key/value pairs:
+%   'verbose'                - Logical, default false
+%   'stdThreshForOutlier'    - How many standard deviations away from the
+%                              mean a value may go to not be considered an
+%                              outlier. Numeric, default 3
+%   'jitterFactor'           - The amount of x-axis jitter in the column
+%                              plots. Numeric (between 0 and 1), default
+%                              0.25
 %
 % Examples:
 %{
@@ -23,26 +37,36 @@
 %}
 
 
-%% Hard-coded variables
-verbose = false; % By default, nothing reported to the console.
-stdThreshForOutlier = 3;
-jitterFactor = .25; % The amount of x-axis jitter in the column plots
-outputDirectory = './';
-closeFigures = true;
+%% Parse vargin for options passed here
+p = inputParser; p.KeepUnmatched = false;
+p.addRequired('theProject', @ischar);
+p.addRequired('targetProjectField', @ischar);
+p.addParameter('verbose',false, @islogical);
+p.addParameter('stdThreshForOutlier',3, @isnumeric);
+p.addParameter('jitterFactor',0.25, @isnumeric);
+p.parse(theProject, targetProjectField, varargin{:})
+
+% Distribute the parameters into variables
+theProject = p.Results.theProject;
+targetProjectField = p.Results.targetProjectField;
+verbose = p.Results.verbose;
+stdThreshForOutlier = p.Results.stdThreshForOutlier;
+jitterFactor = p.Results.jitterFactor;
 
 
-%% Init SDK
+%% Init SDK and Scratch Directory
 api_key = getpref('flywheelMRSupport','flywheelAPIKey');
 fw = flywheel.Flywheel(api_key);
+
+outputDirectory = getpref('flywheelMRSupport','flywheelScratchDir');
 
 
 %% Get the Project we're interested in
 
 % Find a specific Flywheel Project among them all
 all_projects = fw.getAllProjects;
-project_label = 'tome';
 
-project = all_projects{contains(cellfun(@(p) {p.label}, all_projects), project_label)};
+project = all_projects{contains(cellfun(@(p) {p.label}, all_projects), theProject)};
 
 
 %% For each session/acquisition/file find the QA reports and add them to QA
@@ -65,6 +89,8 @@ for ss = 1:numel(project_sessions)
     % For each acquisition, look at all files, if a given file is qa, then
     % add that files info to 'qa'.
     for ii = 1:numel(session_acquisitions)
+        % Iterator to identify duplicate QC files
+        iterator = 1;
         files = session_acquisitions{ii}.files;
         acq_id = session_acquisitions{ii}.id;
         acq_label = session_acquisitions{ii}.label;
@@ -72,6 +98,8 @@ for ss = 1:numel(project_sessions)
         % For each file, if that file is 'qa' get its info metadata
         for ff = 1:length(files)
             if strcmpi(files{ff}.type, 'qa')
+                iterator = iterator + 1;
+                end
                 acq = [];
                 acq.label = acq_label;
                 acq.info = fw.getAcquisitionFileInfo(acq_id, files{ff}.name).info.struct;
@@ -90,7 +118,9 @@ for ss = 1:numel(project_sessions)
                     session.acquisitions{end+1} = acq;
                     FilesAnalyzed{end+1} = files{ff}.name;
                     FilesAnalyzedId{end+1} = acq_id;
-                    break;
+                    if iterator == 3
+                        message = strcat('Acquisition',32,acq_label,32,'in ',32,session.label,32,'for Subject',32,project_sessions{ss}.subject.code,32,'has duplicate QC files.');
+                    warning(message);
                 end
             end
         end
@@ -114,7 +144,7 @@ metricLabels = {'cjv','cnr','efc','fber','wm2max','snr_csf','snr_gm','snr_wm';
 % 2 means any extreme values are bad)
 metricRanges = {0,1,0,1,2,1,1,1;
     0,1,0,1,2,1,1,1;
-    0,0,0,1,1,0,0,0};
+    0,0,0,0,1,0,0,0};
 
 metrics = [];
 dataLabels = [];
@@ -208,7 +238,22 @@ for i = 1:length(mods)
         
         % Place the created axis in the correct position in the figure;
         % each iteration (different metric) gets a different plot in the figure
-        subplot(3,3,iter,axis);
+        sbplt = subplot(3,3,iter,axis);
+        if range == 0 
+            xDim = [sbplt.Position(1)+sbplt.Position(3) sbplt.Position(1)+sbplt.Position(3)];
+            yDim = [sbplt.Position(2) sbplt.Position(2)+sbplt.Position(4)];
+            annotation('textarrow',xDim,yDim,'String','Bad')
+        else
+            if range == 1
+                xDim = [sbplt.Position(1)+sbplt.Position(3) sbplt.Position(1)+sbplt.Position(3)];
+                yDim = [sbplt.Position(2)+sbplt.Position(4) sbplt.Position(2)];
+                annotation('textarrow',xDim,yDim,'String','Bad')
+            else
+                xDim = [sbplt.Position(1)+sbplt.Position(3) sbplt.Position(1)+sbplt.Position(3)];
+                yDim = [sbplt.Position(2) sbplt.Position(2)+sbplt.Position(4)];
+                annotation('doublearrow',xDim,yDim)
+            end
+        end
         
         % Identify the bounds for outlier values for this metric
         if range == 0
@@ -233,7 +278,7 @@ for i = 1:length(mods)
         scatter(axis,x(outsideThreshIdx),values(outsideThreshIdx), 'jitter','on', 'jitterAmount',xMax*jitterFactor,'MarkerFaceColor','r','MarkerEdgeColor','r','MarkerFaceAlpha',.2,'MarkerEdgeAlpha',.2);
         
         % Used for making the outlier structure
-        iterator = 1;
+        final = 1;
         
         % Iterate through the outlier values, identify their properties, and organize them
         % into an 'outlier' structure
@@ -251,11 +296,12 @@ for i = 1:length(mods)
                             % Find the acquisition with the same metric
                             % quantity as the outlier
                             if values(badNums) == QA{NumQA}.acquisitions{NumAcq}.info.(mets{j})
-                                outliers.(mods{i}).(mets{j}).subject{iterator} = QA{NumQA}.subject;
-                                outliers.(mods{i}).(mets{j}).label{iterator} = QA{NumQA}.label;
-                                outliers.(mods{i}).(mets{j}).acquisition{iterator} = QA{NumQA}.acquisitions{NumAcq}.label;
-                                outliers.(mods{i}).(mets{j}).scores{iterator} = values(badNums);
-                                iterator = iterator + 1;
+                                outliers.(mods{i}).(mets{j}).subject{final} = QA{NumQA}.subject;
+                                outliers.(mods{i}).(mets{j}).label{final} = QA{NumQA}.label;
+                                outliers.(mods{i}).(mets{j}).acquisition{final} = QA{NumQA}.acquisitions{NumAcq}.label;
+                                outliers.(mods{i}).(mets{j}).scores{final} = values(badNums);
+                                outliers.(mods{i}).(mets{j}).stdsaway{final} = abs(values(badNums)-nanmean(values))/nanstd(values);
+                                final = final+1;
                             end
                         end
                     end
@@ -270,9 +316,7 @@ for i = 1:length(mods)
     saveas(hFigure,filenameOut);
     
     % Close figure handle
-    if closeFigures
-        close(hFigure);
-    end
+    close(hFigure);
 end
 
 %% Write outlier information to a file (and display, if desired)
@@ -282,6 +326,7 @@ session = {};
 acquisition = {};
 metric = {};
 score = {};
+stdsaway = {};
 modalities = fields(outliers);
 for modalityIdx = 1:length(modalities)
     metricNames = fields(outliers.(modalities{modalityIdx}));
@@ -292,11 +337,12 @@ for modalityIdx = 1:length(modalities)
             acquisition{end+1} = outliers.(modalities{modalityIdx}).(metricNames{metricIdx}).acquisition{finalIdx};
             metric{end+1} = metricNames{metricIdx};
             score{end+1} = outliers.(modalities{modalityIdx}).(metricNames{metricIdx}).scores{finalIdx};
+            stdsaway{end+1} = outliers.(modalities{modalityIdx}).(metricNames{metricIdx}).stdsaway{finalIdx};
         end
     end
 end
-outliersInfo = [subject;session;acquisition;metric;score].';
-T = cell2table(outliersInfo,'VariableNames',{'Subject','Session','Acquisition','Metric','Score'});
+outliersInfo = [subject;session;acquisition;metric;score;stdsaway].';
+T = cell2table(outliersInfo,'VariableNames',{'Subject','Session','Acquisition','Metric','Score','StandardDeviations'});
 OutliersFilename = fullfile(outputDirectory,'outliers.csv');
 writetable(T,OutliersFilename,'Delimiter',',','QuoteStrings',false);
 if verbose
@@ -304,11 +350,26 @@ if verbose
 end
 
 %% Write the output files to the project on Flywheel
-acquisitionArray = repmat({'acquisition'},[1 length(FilesAnalyzed)]);
-file_ref = struct('id', FilesAnalyzedId, 'type', 'acquisition', 'name', FilesAnalyzed);
-analysis = struct('label', 'testAnalysis', 'inputs', {{file_ref}});
-analysisId = fw.addProjectAnalysis(project.id, analysis);
-outputs = {strcat(outputDirectory,'T1w.pdf'), strcat(outputDirectory,'T2w.pdf'), strcat(outputDirectory,'bold.pdf'), strcat(outputDirectory,'outliers.csv')};
-fw.uploadOutputToAnalysis(analysisId, strcat(outputDirectory,outputs));
-cd(outputDirectory);
-delete T1w.pdf T2w.pdf bold.pdf outliers.csv;
+switch targetProjectField
+    case 'analyses'
+        file_ref = {};
+        for NumFiles = 1:length(FilesAnalyzed)
+            file_ref{end+1} = struct('id', FilesAnalyzedId{NumFiles}, 'type', 'acquisition', 'name', FilesAnalyzed{NumFiles});
+        end
+        analysis = struct('label', 'testAnalysis', 'inputs', {file_ref});
+        analysisId = fw.addProjectAnalysis(project.id, analysis);
+        fw.uploadOutputToAnalysis(analysisId, '/private/tmp/flywheel/T1w.pdf');
+        fw.uploadOutputToAnalysis(analysisId, '/private/tmp/flywheel/T2w.pdf');
+        fw.uploadOutputToAnalysis(analysisId, '/private/tmp/flywheel/bold.pdf');
+        fw.uploadOutputToAnalysis(analysisId, '/private/tmp/flywheel/outliers.csv');
+        delete T1w.pdf T2w.pdf bold.pdf outliers.csv;
+    case 'files'
+        cd(outputDirectory);
+        outputs = {'./T1w.pdf', './T2w.pdf', './bold.pdf', './outliers.csv'};
+        for ff = 1:length(outputs)
+            fw.uploadFileToProject(project.id, outputs{ff});
+        end
+        delete T1w.pdf T2w.pdf bold.pdf outliers.csv;
+    otherwise
+        error('Error. Target project field must be either "analyses" or "files".');
+end
