4c33778bb4db3a3614915308ea33e17929ebcc8c
gkaguirre <gkaguirre@me.com>
2018-07-16 11:41:31 -0400

    a bit of detail
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/volumeToSurface.m
+++ code/volumeToSurface.m
@@ -1,4 +1,4 @@
-function [ output_args ] = volumeToSurface( input_args )
+function [ output_args ] = volumeToSurface( input1, input2 )
 % Geneartes a cortical surface map from a nifti volume
 %
 % Description:
@@ -14,6 +14,12 @@ function [ output_args ] = volumeToSurface( input_args )
 %
 % Examples:
 %{
+    a = 1;
+    b = 2;
+    a = 1+2
+    niftiInputPath = '/Users/nikhil/whatever'
+    surfOutputPath = '/Users/nkhil/whatever'
+    volumeToSurface( input_args );
 %}
 
 %% FUNCTION GOES HERE
