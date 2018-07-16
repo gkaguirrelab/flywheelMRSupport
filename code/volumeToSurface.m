4a879c1ce863e1f4ff2063fa6b7ddbdc55df536f
gkaguirre <gkaguirre@me.com>
2018-07-16 11:36:47 -0400

    Starting this
    Signed-off-by: gkaguirre <gkaguirre@me.com>

---
+++ code/volumeToSurface.m
@@ -0,0 +1,23 @@
+function [ output_args ] = volumeToSurface( input_args )
+% Geneartes a cortical surface map from a nifti volume
+%
+% Description:
+%   Uses freesurfer binaries to convert blah blah
+%   http://nben.net/MRI-Geometry/
+%
+% Inputs:
+%   Input1                - Describe the inpit
+%   Input1                - Describe the inpit
+%
+% Outputs:
+%   Output1               - Describe the output
+%
+% Examples:
+%{
+%}
+
+%% FUNCTION GOES HERE
+
+
+end
+
