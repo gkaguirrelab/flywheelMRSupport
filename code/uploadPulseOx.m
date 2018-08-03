e07f8e2bd1f44b66ee64b2032133747032f04d0b
Nikhil Boddu <nikhilvamsi31@gmail.com>
2018-08-03 14:34:54 -0400

    insert comments

--- code/uploadPulseOx.m
+++ code/uploadPulseOx.m
@@ -73,6 +73,10 @@ sessionPaths = p.Results.sessionPaths;
 
 fw = flywheel.Flywheel(getpref('flywheelMRSupport','flywheelAPIKey'));
 
+%% Check to make sure this session is valid
+    if ~contains(availableSessionNames,sessionName)
+        error('This is not a valid session name.');
+    end
 %% Get project ID and sessions
 allProjects = fw.getAllProjects;
 for proj = 1:numel(allProjects)
@@ -81,28 +85,41 @@ for proj = 1:numel(allProjects)
     end
 end
 
+% Get all the sessions for this project
 allSessions = fw.getProjectSessions(projID);
 for ss = 1:length(allSessions)
+    % Find the session we are looking for
     if strcmp(allSessions{ss}.label,sessionName) && strcmp(allSessions{ss}.subject.code,subjectName)
         session = allSessions{ss};
         break;
     end
 end
 
+% Get the session ID
 sesID = session.id;
+
+% Get all acquisitions for this session
 allAcqs = fw.getSessionAcquisitions(sesID);
+
 % Instantiate arrays
 acqTimes = NaT(1,'TimeZone','America/New_York');
 acqIDs = {};
 idx = 0;
+
 % Get the desired acquisitions
 for acq = 1:numel(allAcqs)
+    
+    % Only find tfMRI and rfMRI runs for PulseOx (no SBRefs)
     if (contains(allAcqs{acq}.label,'tfMRI') || contains(allAcqs{acq}.label,'rfMRI')) && ~contains(allAcqs{acq}.label,'SBRef')
         idx = idx+1;
+        
+        % Convert to a valid timestamp that MATLAB understands
         newAcqTime = allAcqs{acq}.timestamp;
         newAcqTime = erase(newAcqTime,extractAfter(newAcqTime,16));
         newAcqTime = strcat(newAcqTime,'+00:00');
         newAcqTime = datetime(newAcqTime,'InputFormat','yyyy-MM-dd''T''HH:mmXXX','TimeZone','America/New_York');
+        
+        % Add acquisition data to arrays
         acqTimes(idx) = newAcqTime;
         acqIDs{end+1} = allAcqs{acq}.id;
     end
@@ -116,7 +133,7 @@ if ~isempty(sortedIDs)
         sortedIDs{ii} = acqIDs{ind(ii)};
     end
     
-    % Check for Session 1
+    % Find the right subject/session directory
     for ssn = 1:length(availableSessionNames)
         if strcmp(sessionName,availableSessionNames{ssn})
             % Find the Subject Directory
@@ -135,10 +152,12 @@ if ~isempty(sortedIDs)
         end
     end
     
+    % Find all the PulseOx files for this session
     pulseDir = fullfile(subjDir, sesDir.name,'ScannerFiles','PulseOx');
     allPulseFiles = dir(fullfile(pulseDir));
     pulseFiles = allPulseFiles(3:end);
     
+    % Get the current acquisition and check for existing PulseOx log files
     for file = 1:length(sortedIDs)
         acqToUpload = fw.getAcquisition(sortedIDs{file});
         acqToUploadFiles = acqToUpload.files;
@@ -149,37 +168,52 @@ if ~isempty(sortedIDs)
             end
         end
         
+        % Warning if log files already exist.
         if logExists
             warning('This session already has PulseOx files on Flywheel.');
         end
     end
     
+    % Create the directory to store the dicom files.
     scratchDir = getpref('flywheelMRSupport','flywheelScratchDir');
     if ~exist(fullfile(scratchDir,'dicomFiles'))
         mkdir(fullfile(scratchDir,'dicomFiles'));
     end
     
+    % Create a directory to store output of PulseResp (comparing dicoms and
+    % PulseOx log files).
     if ~exist(fullfile(scratchDir,'dicomFiles/pulseOutput'))
         mkdir(fullfile(scratchDir,'dicomFiles/pulseOutput'));
     end
     
+    % Instantiate outputs
     pulseOxMatches = strings(length(sortedIDs),2);
     allMatched = true;
+    
+    % Find the dicom files and download them to the dicom directory.
     for file = 1:length(sortedIDs)
         acqToUpload = fw.getAcquisition(sortedIDs{file});
         pulseOxMatches(file,1) = acqToUpload.label;
         acqToUploadFiles = acqToUpload.files;
         for fff = 1:length(acqToUploadFiles)
+            
+            % Finding the dicom file
             if strcmp(acqToUploadFiles{fff}.type,'dicom')
                 if verbose
                     disp(acqToUploadFiles{fff}.name);
                 end
                 dcmFile = strcat(scratchDir,'/dicomFiles/',acqToUploadFiles{fff}.name);
+                
+                % Downloading the file
                 if ~exist(dcmFile)
                     dcmFile = fw.downloadFileFromAcquisition(acqToUpload.id,acqToUploadFiles{fff}.name,dcmFile);
                 end
                 dcmDir = strcat(scratchDir,'/dicomFiles/dicomDir');
+                
+                % Unzip the file
                 unzip(dcmFile,dcmDir);
+                
+                % Special case, some files are unzipped differently. This fixes that bug. 
                 if contains(dcmFile,'.dicom')
                     dirs = dir(dcmDir);
                     extraDirName = dirs(3).name;
@@ -190,28 +224,36 @@ if ~isempty(sortedIDs)
             end
         end
         
+        % Go through the PulseOx files and compare them to the dicoms.
         for jj = 1:length(pulseFiles)
             pulseFile = strcat(pulseFiles(jj).folder,'/',pulseFiles(jj).name);
             try
+                % Compare the dicoms and the log file
                 PulseResp(dcmDir,pulseFile,strcat(scratchDir,'/dicomFiles/pulseOutput'),'verbose',verbose);
                 if verbose
                     disp('this works.');
                 end
+                % If they are matched, upload the log file to Flywheel
                 fw.uploadFileToAcquisition(sortedIDs{file},pulseFile);
+                
+                % Matching the PulseOx file to the corresponding
+                % acquisition
                 pulseOxMatches(file,2) = pulseFiles(jj).name;
                 break;
-            catch ME
+            catch 
                 if verbose
                     disp('this does not work.');
                 end
+                
+                % Warning if no log files match this acquisition
                 if jj == length(pulseFiles)
                     warning(['Aquisition ' acqToUpload.label ' has no compatible PulseOx files']);
                     allMatched = false;
                 end
-                rethrow(ME);
             end
         end
         
+        % Cleaning up the dicom directory by deleting all files inside it
         testDir = dir(dcmDir);
         if ~testDir(3).isdir
             allDcmFiles = fullfile(dcmDir,'/*');
@@ -222,7 +264,10 @@ if ~isempty(sortedIDs)
             rmdir(fullfile(dcmDir,testDir(3).name));
         end
         
+        % Delete the dicom directory.
         rmdir(dcmDir);
+        
+        % Delete the zipped dicom file.
         delete(dcmFile);
         
     end
